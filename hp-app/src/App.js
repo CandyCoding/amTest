a16c177dee11d1c013de3c55a1493c62885b3262
CandyCoding <candy.lopez.gr@gmail.com>
2022-07-18 13:31:29 -0500

    Proyecto en React iniciado, Rest API generada, inicio de maquetación, sin estilos

---
+++ hp-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Harry Potter</h1>
+    <h2>selecciona tu filtro</h2>
+    <button>Estudiantes</button>
+    <button>Staff</button>
+</div>
+);
+}
+
+export default App;
