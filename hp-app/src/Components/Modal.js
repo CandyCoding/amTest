4b9b0dfec6ef47308eafcee34b0751eeaaeca04b
CandyCoding <candy.lopez.gr@gmail.com>
2022-07-20 02:15:01 -0500

    Actualización de función de agregar personajes a la base de datos, pendiente subir archivo de fotografía y condicionar posición del personaje

--- hp-app/src/Components/Modal.js
+++ hp-app/src/Components/Modal.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 export default function Modal ({children, handleClose}) {
     const [newCharacter, setNewCharacter] = useState(
-        {
+        { 
             name: "",
             house: "",
             gender: "",
@@ -9,9 +9,32 @@ export default function Modal ({children, handleClose}) {
             eyeColour: "",
             hairColour: "",
             image: "",
-            id: ""
+            position:""
         }
     )
+const handleChange= (e) => {
+    setNewCharacter({
+        ...newCharacter,
+        [e.target.name]: e.target.value
+    })
+}
+const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3001/characters', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newCharacter),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            handleClose();
+        }
+        )
+}
+
     return (
         <div className="modal">
             <div className="modal_content">
@@ -21,13 +44,13 @@ export default function Modal ({children, handleClose}) {
                 </div>
                 <div className="modal_body">
                     <label>NOMBRE</label>
-                    <input type="text" className="modal_input"/>
+                    <input type="text" className="modal_input" name="name" onChange={handleChange}/>
                     <label>CUMPLEAÑOS</label>
-                    <input type="text"className="modal_input"/>
+                    <input type="text"className="modal_input" name= "dateOfBirth"onChange={handleChange}/>
                     <label>COLOR DE OJOS</label>
-                    <input type="text"className="modal_input"/>
+                    <input type="text"className="modal_input" name="eyeColour"onChange={handleChange}/>
                     <label>COLOR DE CABELLO</label>
-                    <input type="text"className="modal_input"/>
+                    <input type="text"className="modal_input" name="hairColour"onChange={handleChange}/>
                     </div>
                     <div className="modal_labels">
                     <label>GÉNERO</label>
@@ -35,19 +58,19 @@ export default function Modal ({children, handleClose}) {
                     </div>
                     <div className="modal_checks">
                     <label>Mujer</label>
-                    <input type="radio" checked/>
+                    <input type="radio" checked name="gender" value="female"onChange={handleChange}/>
                     <label>Hombre</label>
-                    <input type="radio"/>
+                    <input type="radio"  name="gender" value="male"onChange={handleChange} />
                     <label>Estudiante</label>
-                    <input type="radio" checked/>
+                    <input type="radio" name="position" value="hogwartsStudent"onChange={handleChange}/>
                     <label>Staff</label>
-                    <input type="radio"/>
+                    <input type="radio"checked name="position" value="hogwartsStaff"onChange={handleChange}/>
                     </div>
                     <div className="modal_files">
                     <label>Fotografía</label>
                     <input type="file"/>
                     </div>
-                    <button className="keep_btn">GUARDAR</button>
+                    <button className="keep_btn" onClick={handleSubmit}>GUARDAR</button>
             {children}
             </div>
         </div> 
